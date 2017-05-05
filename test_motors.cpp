adaac845977d2e885fbff5e820f1b433c8d019b9
Benjamin Secker <benjamin.secker@gmail.com>
2017-05-05 05:13:09 +0000

    changed test motors to have turn_left function

--- test_motors.cpp
+++ test_motors.cpp
@@ -2,8 +2,21 @@
 #include <time.h>
 #include "E101.h"
 
+void turnleft( int duration, int speed) {
+	// set left motor to -speed and right motor to speed
+	set_motor(1, -speed);
+	set_motor(2, speed);
+	
+	// sleep for specified time
+	sleep1(0, duration);
+	
+	// stop motors
+	stop(1);
+	stop(2);
+}
+
 int main() {
 	init();
-	set_motor(1, 255);
-	set_motor(2, 255);
+	turnleft(500000, 255);
+	return 0;
 }
