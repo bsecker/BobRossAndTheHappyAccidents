358cfcd85732a6f3820dff3d16220ab57c495877
Benjamin <benjamin.secker@gmail.com>
2017-05-07 22:22:03 +1200

    minor changes to copy_rpi.sh. still working

--- copy_rpi.sh
+++ copy_rpi.sh
@@ -2,16 +2,17 @@
 
 # Potentially dangerous shell script to wipe the RPi, copy latest code from source PC and recompile.
 
+
 # arguments:
 # copy_rpi.sh [source folder]
 
-IP = pi@10.140.120.159
-DIRPI = /home/pi/AVC/
-DIRPC = $1
+IP=pi@10.140.120.159
+DIRPI=/home/pi/AVC/
+DIRPC=$1
 
 echo "Deleting old c files"
 ssh -x $IP 'cd $DIRPI; rm *.cpp'
-echo "Transferrin files to pi"
+echo "Transferring files to pi"
 rsync -e 'ssh -ax' -av $DIRPC $IP:$DIRPI
 echo "compiling code"
 ssh -x $IP 'cd $DIRPI; make; ./start'
