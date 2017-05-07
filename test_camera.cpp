739a375f7b4fcccc97844b3774d034d874b7f886
B=Benjamin Secker <benjamin.secker@gmail.com>
2017-05-07 22:47:26 +1200

    testing camera line getting

--- test_camera.cpp
+++ test_camera.cpp
@@ -14,18 +14,22 @@ int main() {
 
 	int cameraWidth = 320;
 	int cameraHeight = 240;
+
+	int cameraLine[ CAMERA_WIDTH ];
+
 		
 	
 
 	// get line of pixels
-	for (int _i = 0; _i < 320, _i ++){
+	for (int _i = 0; _i < CAMERA_WIDTH, _i ++){
 		char pix = get_pixel( 160, 120, 3);
-	
+		cameraLine[ i ] = pix;	
 	}
 
 
 	//display_picture(50, 500000);
-	//printf("%d\n", pix);
+	printf("%d\n", cameraLine[ CAMERA_WIDTH/2 ] );
+	
 	
 	sleep1(0,500000);	
 	return 0;
