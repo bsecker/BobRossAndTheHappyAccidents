aeebf4cc2a97690d22e7f107d27e455795aac38e
Mathew Scott <scottclan0002@gmail.com>
2017-05-19 15:58:06 +1200

    Added complicated error calculating alghorithm

--- IR_functions.cpp
+++ IR_functions.cpp
@@ -1,1 +1,24 @@
-hi matt
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+
+// return average of three sensor readings.
+int read_IR(int a) {
+	int read1;
+	int read2;
+	int read3;
+	int average;
+	 read1 = read_analog(a);
+	 sleep1(0,500);
+	 read2 = read_analog(a);
+	 sleep(0,500);
+	 read3 = read_analog(a);
+	 average = ((read1+read2+read3)/3);
+  
+	return average;
+}
+int getIR_error(int left_reading, int right_reading){
+	int IR_error=(right_reading-left_reading);
+	return IR_error
+}
